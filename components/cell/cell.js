e2db68a951340c92cf3cbdadf151a5bfb23c177e
rock506 <wurongle@gmail.com>
2016-03-18 17:44:32 +0800

    Create cell.js

---
+++ components/cell/cell.js
@@ -0,0 +1,30 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ */
+'use strict';
+import React, {
+  Component,
+  StyleSheet,
+  Text,
+  View,
+  TouchableHighlight
+} from 'react-native';
+
+class Cell extends Component {
+  render() {
+    return (
+      <TouchableHighlight>
+        <Text>
+          开发中...
+        </Text>
+      </TouchableHighlight>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+
+});
+
+module.exports = Cell;
