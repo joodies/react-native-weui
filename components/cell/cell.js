f8b2f8c76355bcffb3d628255165c9be89d80ae5
rock506 <wurongle@gmail.com>
2016-03-31 21:27:47 +0800

    Update cell.js

--- components/cell/cell.js
+++ components/cell/cell.js
@@ -1,70 +1,30 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- */
 'use strict';
 import React, {
-  Component,
-  StyleSheet,
-  Text,
-  View,
-  TouchableHighlight
+    Component,
+    StyleSheet,
+    View
 } from 'react-native';
 
-class Cell extends Component {
-  render() {
-    var touchableProps = {};
+export default class Cell extends Component {
+    render() {
+        var {children,...others} = this.props;
 
-    if (!this.props.disabled) {
-      touchableProps.onPress = this.props.onPress;
-      touchableProps.onPressIn = this.props.onPressIn;
-      touchableProps.onPressOut = this.props.onPressOut;
-      touchableProps.onLongPress = this.props.onLongPress;
+        return (
+            <View {...others} style={styles.cell}>
+                <View style={styles.inner}>{children}</View>
+            </View>
+        );
     }
-    
-    return (
-      <TouchableHighlight {...touchableProps} underlayColor={underlayColor}>
-        <View style={[styles.baseCell,this.props.style]}>
-          <View style={styles.hd}></View>
-          <View style={styles.bd}><Text style={styles.text}>Cell</Text></View>
-          <View style={styles.ft}>
-            <View style={styles.access}></View>
-          </View>
-        </View>
-      </TouchableHighlight>
-    );
-  }
 }
 
 const styles = StyleSheet.create({
-  baseCell: {
-    borderTopWidth: 0.5,
-    borderBottomWidth: 0.5,
-    flexDirection: 'row',
-    borderColor: '#D9D9D9',
-  },
-  hd:{
-  },
-  bd:{
-    flex:1,
-  },
-  ft:{
-    justifyContent:'center',
-    flexDirection: 'row',
-    paddingRight:5
-  },
-  text: {
-    fontSize: 18,
-    margin: 10,
-  },
-  access:{
-    borderRightWidth:2,
-    borderTopWidth:2,
-    borderColor:'#C8C7CD',
-    transform: [{rotate: '45deg'},{translateY:5}],
-    width:10,
-    height:10,
-  }
+    cell: {
+        paddingLeft:15
+    },
+    inner:{
+        borderBottomWidth: 0.5,
+        flexDirection: 'row',
+        borderColor: '#D9D9D9',
+        alignItems: 'center',
+    }
 });
-
-module.exports = Cell;
